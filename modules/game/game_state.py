c15b60248c9e77a49ef2c02843712d0275406921
lobana <tadroslobana@gmail.com>
2026-07-02 20:25:54 +0300

    GameState

--- modules/game/game_state.py
+++ modules/game/game_state.py
@@ -1,24 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional
 
-# "يحفظ حالة المباراة."
-
-
-
-
-
-
-
-# /*
-# GameState
-# │
-# ├── is_running
-# ├── is_finished
-# ├── current_turn
-# ├── winner
-# ├── game_mode
-# └── difficulty
-
-# */
+from modules.shared.enums.difficulty import Difficulty
+from modules.shared.enums.game_mode import GameMode
+from modules.shared.enums.game_status import GameStatus
+from modules.shared.enums.player_color import PlayerColor
 
 
+@dataclass
+class GameState:
+    """
+    Represents the current state of a chess game.
 
+    This class stores all game-related data during a match.
+    It does not contain game logic.
+    """
 
+    game_mode: GameMode
+    status: GameStatus
+    current_turn_color: PlayerColor
+    difficulty: Optional[Difficulty] = None
+    winner: Optional[PlayerColor] = None
